30366b1710781f5e933e038282c5259d390cc26a
chr <chengran@viewhigh,com>
2020-11-08 22:32:23 +0800

    commit

--- src/main/java/com/ccccccc/pizzademo/controller/DesignController.java
+++ src/main/java/com/ccccccc/pizzademo/controller/DesignController.java
@@ -6,10 +6,12 @@ import com.ccccccc.pizzademo.domain.Pizza;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -49,7 +51,10 @@ public class DesignController {
     }
 
     @PostMapping
-    public String processDesign(Pizza design){
+    public String processDesign(@Valid Pizza design, Errors errors){
+        if (errors.hasErrors())
+            return "design";
+
         log.info("Process design: "+design);
         return "redirect:/orders/current";
     }
--- src/main/java/com/ccccccc/pizzademo/domain/Pizza.java
+++ src/main/java/com/ccccccc/pizzademo/domain/Pizza.java
@@ -2,10 +2,18 @@ package com.ccccccc.pizzademo.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Pizza {
+
+    @NotNull
+    @Size(min = 5, message = "Name must be more than 5 characters")
     private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at least one ingredient")
     private List<String> ingredients;
 }
