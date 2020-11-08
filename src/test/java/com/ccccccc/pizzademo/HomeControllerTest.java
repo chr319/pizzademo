519a66c64c53d1cd29c4ce91b569c3c6c3357bc5
chr <chengran@viewhigh,com>
2020-11-08 22:43:57 +0800

    commit

--- src/test/java/com/ccccccc/pizzademo/HomeControllerTest.java
+++ src/test/java/com/ccccccc/pizzademo/HomeControllerTest.java
@@ -1,11 +1,9 @@
 package com.ccccccc.pizzademo;
 
-import com.ccccccc.pizzademo.controller.HomeController;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -13,7 +11,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
     @Autowired
