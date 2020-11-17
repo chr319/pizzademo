1daafc6c11e3f9f5fe2d0ed0af7ae48f188384a4
chengran <chengran@viewhigh.com>
2020-11-17 19:05:23 +0800

    commit

--- src/main/java/com/ccccccc/pizzademo/data/OrderRepository.java
+++ src/main/java/com/ccccccc/pizzademo/data/OrderRepository.java
@@ -3,5 +3,8 @@ package com.ccccccc.pizzademo.data;
 import com.ccccccc.pizzademo.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findByDeliveryZip(String deliveryZip);
 }
