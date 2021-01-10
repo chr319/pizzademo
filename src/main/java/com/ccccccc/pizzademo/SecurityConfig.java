8001ab0ea15a957ec8be26a9a410c8d97fa7f502
chr <chengran@viewhigh,com>
2021-01-10 18:15:33 +0800

    commit

--- src/main/java/com/ccccccc/pizzademo/SecurityConfig.java
+++ src/main/java/com/ccccccc/pizzademo/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.ccccccc.pizzademo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,20 +16,39 @@ import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Qualifier("inMemoryUserDetailsManager")
+
+    @Qualifier("userRepositoryUserDetailsService")
     @Autowired
     private UserDetailsService userDetailsService;
 
-    public PasswordEncoder encoder(){
+    @Bean
+    public PasswordEncoder encoder() {
         return new SCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-        .userDetailsService(userDetailsService)
-        .passwordEncoder(encoder());
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .hasRole("USER")
+                .antMatchers("/", "/**")
+                .permitAll()
 
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+    }
 }
